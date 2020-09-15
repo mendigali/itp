9839788ead0b7c78e15e1604add4ac1c5880b974
Mendigali Temir <51957278+mendigali@users.noreply.github.com>
2020-09-16 00:09:11 +0600

    Previous commit was bug. So this is second attempt

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void solve() {
+
+}
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int test;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
+    return 0;
+}
