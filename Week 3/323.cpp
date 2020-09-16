e8352bf86b0741b587bccbe112d43ad758fbe169
Mendigali Temir <51957278+mendigali@users.noreply.github.com>
2020-09-16 23:24:50 +0600

    323 edited

--- Week 3/323.cpp
+++ Week 3/323.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main() {
     double x;
     cin >> x;
-    double result = x * x, xx = x * x;
+    double result = x * x;
     int numerator = 256;
     while (numerator >= 2) {
-        result = (numerator / result) + xx;
+        result = (numerator / result) + x * x;
         numerator /= 2;
     }
     cout << x / result;
