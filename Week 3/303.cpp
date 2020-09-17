66423971dd0eb8dcc0800e77ec7dd6888c82526d
Mendigali Temir <51957278+mendigali@users.noreply.github.com>
2020-09-17 13:03:00 +0600

    Changes

--- Week 3/303.cpp
+++ Week 3/303.cpp
@@ -2,18 +2,17 @@
 #include <cmath>
 using namespace std;
 int main() {
-    double D, a, b, c, x1, x2;
+    double a, b, c;
     cin >> a >> b >> c;
-    D = b * b - 4 * a * c;
-    if (D > 0) {
-        x1 = (-b + sqrt(D)) / (2 * a);
-        x2 = (-b - sqrt(D)) / (2 * a);
-        cout << x1 << " " << x2 << endl;
-    } else if (D == 0) {
-        x1 = (-b + sqrt(D)) / (2 * a);
-        cout << x1 << endl;
-    } else if (D < 0) {
-        cout << "no solution" << endl;
+    double d = b * b - 4 * a * c;
+    double x1 = (-b + sqrt(d)) / (2 * a);
+    if (d > 0) {
+        double x2 = (-b - sqrt(d)) / (2 * a);
+        cout << x1 << ' ' << x2 << '\n';
+    } else if (d == 0) {
+        cout << x1 << '\n';
+    } else if (d < 0) {
+        cout << "no solution" << '\n';
     }
     return 0;
 }
